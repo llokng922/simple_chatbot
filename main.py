95f2daec07df3a910ce7c333fc9ba0dcdcfff17d
llokng922 <llokng922@gmail.com>
2020-02-11 11:04:32 +0800

    MacbkAir Feb11

--- main.py
+++ main.py
@@ -65,19 +65,20 @@ except:
     with open("data.pickle","wb") as f:
         pickle.dump((words, labels, training, output),f)
 
-tf.reset_default_graph()
 
-net = tflearn.input_data(shape=[None,len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]),activation="softmax")
-net = tflearn.regression(net)
-
-model = tflearn.DNN(net)
 
 try:
     model.load("model.tflearn")
 except:
+    tf.reset_default_graph()
+
+    net = tflearn.input_data(shape=[None,len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]),activation="softmax")
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
     model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
